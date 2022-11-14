70263d49fe91ca554080bd01cc884ca7a5a9ae3e
zadubanm02 <zadubanm02@gmail.com>
2022-11-14 21:03:28 +0100

    Remove unnecesary

--- pages/friends/index.tsx
+++ pages/friends/index.tsx
@@ -1,14 +1,7 @@
 import React, { Suspense, useState } from "react";
 import Navbar from "../../components/general/Navbar";
-import dynamic from "next/dynamic";
-import Calendar from "react-calendar";
-import "react-calendar/dist/calendar.css";
-import styled from "styled-components";
-import Banner from "../../components/dashboard/Banner";
-import Friends from "../../components/dashboard/Friends";
 import { NextPage } from "next";
 import { useFriends } from "../../hooks/useFriends";
-import FriendRow from "../../components/general/FriendRow";
 import RowFriend from "../../components/new/RowFriend";
 
 // const Calendar = dynamic(() => import("react-calendar"), {
