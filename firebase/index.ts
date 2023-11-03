faf7c4eb2770e7661aff909abed77d0d258c28bc
Michal Zaduban <51057141+zadubanm02@users.noreply.github.com>
2023-11-03 13:45:04 +0100

    Update index.ts

--- firebase/index.ts
+++ firebase/index.ts
@@ -9,13 +9,13 @@ import { getMessaging, getToken } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC0Gr5iGJmt3vHGV3_SuuV1lH8Iv2ukm18",
-  authDomain: "trapp-8f7cf.firebaseapp.com",
-  projectId: "trapp-8f7cf",
-  storageBucket: "trapp-8f7cf.appspot.com",
-  messagingSenderId: "654035320364",
-  appId: "1:654035320364:web:70787ed6e6708a556adb89",
-  measurementId: "G-VZ6Y79RJQL",
+  apiKey: "<>",
+  authDomain: "<>",
+  projected: "<>",
+  storageBucket: "<>",
+  messagingSenderId: "<>",
+  appId: "<>",
+  measurementId: "<>"
 };
 
 // Initialize Firebase
